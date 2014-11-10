9c1b06655041b1f8613c22a4c66642c2065ec40d
Bookis Smuin <bookis.smuin@gmail.com>
2014-11-10 11:19:08 -0800

    change create to save

--- spec/lib/repo_man_spec.rb
+++ spec/lib/repo_man_spec.rb
@@ -3,10 +3,10 @@ require "./lib/repo_man"
 describe RepoMan do
   let(:repo_man) { RepoMan.new("Cooks", "Bookis") }
   let(:repo_options) { {organization: "Ada-Projects", auto_init: true} }
-  describe '#create' do
+  describe '#save' do
     it 'requests to github' do
       expect(Octokit).to receive(:create_repository).with("Cooks-Bookis", repo_options)
-      repo_man.create
+      repo_man.save
     end
   end
 end
